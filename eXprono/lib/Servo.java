cc6a87a6e330baa280725d5664531f7db84c3f59
Anno van den Akker <annovda@gmail.com>
2014-10-28 01:16:40 -0400

    Many board images

---
+++ eXprono/lib/Servo.java
@@ -0,0 +1,30 @@
+package eXprono.lib;
+
+public class Servo {
+	
+	private int pin = 0;
+	private int angle = 0;
+	private int pulse = 0;
+	private int minPulse = 34;
+	private int maxPulse = 150;
+	
+	public Servo() {
+		
+	}
+	
+	public void setMinimumPulse(int t) {
+		
+	}
+	
+	public void setMaximumPulse(int i) {
+		
+	}
+	
+	public void attach(int pin) {
+		
+	}
+	
+	public void write(int angle) {
+		
+	}
+}
